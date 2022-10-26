6774c28ee8ba87667c971fac329caec205c65bfe
lxy <852559563@qq.com>
2022-10-26 20:10:55 +0800

    accomplish a*

--- astar.py
+++ astar.py
@@ -1,23 +1,24 @@
 import math
 import operator
+import time
 from random import randint
 import numpy as np
 from enum import Enum
 from matplotlib import pyplot as plt
-from shapely import geometry
+import pandas as pd
 
 # 定义全局变量
 CELL_WIDTH = 1  # 单元格宽度
 CELL_HEIGHT = 1  # 单元格长度
 BLOCK_NUM = 60  # 地图中的障碍物数量
-BLOCK_BIG_NUM = 10  # 地图中大障碍物数量
-BLOCK_BORD = 50  # 地图中障碍块最大边长
+BLOCK_BIG_NUM = 8  # 地图中大障碍物数量
+BLOCK_BORD = (20, 40)  # 地图中障碍块最大边长
 V0 = 15  # 初速度
-COST_RATE = 0.9  # 转弯之后的速度变为原来的0.9  如果损失设置太小方向会转不过来
+COST_RATE = 1  # 转弯之后的速度变为原来的0.9  如果损失设置太小方向会转不过来
 
 
-def draw_path(mapsize, blocklist, routelist, turnlist):
-    plt.figure(figsize=(mapsize[0], mapsize[1]))  # 为了防止x,y轴间隔不一样长，影响最后的表现效果，所以手动设定等长
+def draw_path_from_csv(mapsize):
+    plt.figure(figsize=(100, 100))  # 为了防止x,y轴间隔不一样长，影响最后的表现效果，所以手动设定等长
     plt.xlim(-1, mapsize[0])
     plt.ylim(-1, mapsize[1])
     my_x_ticks = np.arange(0, mapsize[0], 1)
@@ -25,18 +26,49 @@ def draw_path(mapsize, blocklist, routelist, turnlist):
     plt.xticks(my_x_ticks)  # 我理解为竖线的位置与间隔
     plt.yticks(my_y_ticks)
     plt.grid(True)  # 开启栅格
-    sub=np.array([(0.5,0.5)])
-    # routelist = np.array(routelist)
-    blocklist = np.subtract(np.array(blocklist),sub)
-    turnlist = np.subtract(np.array(turnlist),sub)
-    # plt.plot(routelist[:, 0], routelist[:, 1], linewidth=3)
-    plt.plot(turnlist[:, 0], turnlist[:, 1], linewidth=3)
-    plt.scatter(blocklist[:, 0], blocklist[:, 1], s=2700, c='k', marker='s')
-    plt.title("grid map simulation ")
 
-    # plt.show()
-    plt.savefig('result.png',dpi=100)
+    route1 = np.array(read_list_from_file("./route_rate1.csv"))
+    route0dot9 = np.array(read_list_from_file("./route_rate0.9.csv"))
+    route0dot6 = np.array(read_list_from_file("./route_rate0.6.csv"))
+    blocklist = read_list_from_file("./block.csv")
+    sub = np.array([(0.5, 0.5)])
+    blocklist = np.subtract(np.array(blocklist), sub)
+
+    plt.plot(route1[:, 0], route1[:, 1], linewidth=2, label="no velocity loss")
+    plt.plot(route0dot9[:, 0], route0dot9[:, 1], linewidth=2, label="loss_rate: 0.9")
+    plt.plot(route0dot6[:, 0], route0dot6[:, 1], linewidth=2, label="loss_rate: 0.6")
+    plt.scatter(blocklist[:, 0], blocklist[:, 1], s=600, c='k', marker='s')
+    plt.scatter(route1[0][0], route1[0][1], s=600, label="start_point")
+    plt.scatter(route1[len(route1) - 1][0], route1[len(route1) - 1][1], s=600, label="end_point")
+    plt.title("comparison")
+    plt.legend()
+    plt.savefig('route_comparison.png', dpi=100)
     plt.clf()
+
+
+def draw_path(mapsize, blocklist, routelist, turnlist):
+    plt.figure(figsize=(100, 100))  # 为了防止x,y轴间隔不一样长，影响最后的表现效果，所以手动设定等长
+    plt.xlim(-1, mapsize[0])
+    plt.ylim(-1, mapsize[1])
+    my_x_ticks = np.arange(0, mapsize[0], 1)
+    my_y_ticks = np.arange(0, mapsize[1], 1)
+    plt.xticks(my_x_ticks)  # 我理解为竖线的位置与间隔
+    plt.yticks(my_y_ticks)
+    plt.grid(True)  # 开启栅格
+    sub = np.array([(0.5, 0.5)])
+    routelist = np.subtract(np.array(routelist), sub)
+    blocklist = np.subtract(np.array(blocklist), sub)
+    turnlist = np.subtract(np.array(turnlist), sub)
+    plt.plot(routelist[:, 0], routelist[:, 1], linewidth=2)
+    plt.plot(turnlist[:, 0], turnlist[:, 1], linewidth=2)
+    plt.scatter(blocklist[:, 0], blocklist[:, 1], s=600, c='k', marker='s')
+    plt.title("grid map simulation ")
+    plt.show()
+
+    # save_to_file(routelist, "route_rate1.csv")
+    # save_to_file(turnlist, "turn_rate1.csv")
+    # plt.savefig('result_rate1.png', dpi=100)
+    # plt.clf()
 
 
 # 由两个点求得直线公式：AX+BY+C=0
@@ -57,33 +89,12 @@ def block_exist(node1, node2, blocklist):
             # 相交返回True
             # print(node1, node2, cur_node)
             A, B, C = get_line(node1, node2)
-            peaks = []
-            peaks.append(np.array((cur_node[0] - CELL_WIDTH / 2, cur_node[1] - CELL_HEIGHT / 2, 1)))
-            peaks.append(np.array((cur_node[0] + CELL_WIDTH / 2, cur_node[1] - CELL_HEIGHT / 2, 1)))
-            peaks.append(np.array((cur_node[0] - CELL_WIDTH / 2, cur_node[1] + CELL_HEIGHT / 2, 1)))
-            peaks.append(np.array((cur_node[0] + CELL_WIDTH / 2, cur_node[1] + CELL_HEIGHT / 2, 1)))
-            weights = np.array((A, B, C))
-            # print("judge", np.dot(peaks[0], weights), np.dot(peaks[1], weights), np.dot(peaks[2], weights),
-            #       np.dot(peaks[3], weights))
-            # 四个顶点代入直线是否同号
-            if np.dot(peaks[0], weights) <= 0 and np.dot(peaks[1], weights) <= 0 and np.dot(peaks[2],
-                                                                                            weights) <= 0 and np.dot(
-                peaks[3], weights) <= 0 or \
-                    np.dot(peaks[0], weights) >= 0 and np.dot(peaks[1], weights) >= 0 and np.dot(peaks[2],
-                                                                                                 weights) >= 0 and np.dot(
-                peaks[3], weights) >= 0:
-                pass
-            else:
+            # 用点到直线的距离计算
+            distance = np.abs(A * cur_node[0] + B * cur_node[1] + C) / (np.sqrt(A ** 2 + B ** 2))
+            if distance <= math.sqrt(2) * CELL_WIDTH / 2:
                 return True
 
     return False
-
-
-def if_inPoly(polygon, Points):
-    line = geometry.LineString(polygon)
-    point = geometry.Point(Points)
-    polygon = geometry.Polygon(line)
-    return polygon.contains(point)
 
 
 def init_orientation(snode, enode):
@@ -146,19 +157,22 @@ class Node(object):
         if father == None:
             print('未设置当前节点的父节点！')
 
-        gx_father = father.gvalue
-        # 采用欧式距离计算父节点到当前节点的距离
-        gx_f2n = math.sqrt((father.pos[0] - self.pos[0]) ** 2 + (father.pos[1] - self.pos[1]) ** 2)
-        gvalue = gx_f2n + gx_father  # gvalue代表累加距离
-        # print("father:", father.pos, father.offset, father.velocity, "cur:", self.pos, self.offset, self.velocity)
-        hx_n2enode = math.sqrt(
-            (self.pos[0] - enode.pos[0]) ** 2 + (self.pos[1] - enode.pos[1]) ** 2)  # hx_n2enode代表当前点距离目标点的距离
-        fvalue = gvalue + hx_n2enode  # 计算总距离
         if self.offset != father.offset:  # 如果当前节点需要改变方向，则速度损失，如果不需要改变方向，则保持原速度
             self.velocity = father.velocity * COST_RATE
         else:
             self.velocity = father.velocity
-        fvalue /= self.velocity  # fvalue代表所用时间 时间=走过距离+估算的欧式距离/当前速度
+        gx_father = father.gvalue
+        # 采用欧式距离计算父节点到当前节点的距离
+        gx_f2n = math.sqrt((father.pos[0] - self.pos[0]) ** 2 + (father.pos[1] - self.pos[1]) ** 2)
+        # print(gx_f2n,self.velocity)
+        gvalue = gx_f2n / self.velocity + gx_father  # gvalue代表累加距离
+        # print("father:", father.pos, father.offset, father.velocity, "cur:", self.pos, self.offset, self.velocity)
+        hx_n2enode = math.sqrt(
+            (self.pos[0] - enode.pos[0]) ** 2 + (self.pos[1] - enode.pos[1]) ** 2)  # hx_n2enode代表当前点距离目标点的距离
+        # hx_n2enode = math.sqrt(2) * max(abs(self.pos[0] - enode.pos[0]), abs(self.pos[1] - enode.pos[1]))  # 启发函数改为切比雪夫
+        # fvalue = gvalue + hx_n2enode  # 计算总距离
+
+        fvalue = gvalue + hx_n2enode / self.velocity  # fvalue代表所用时间 时间=走过距离+估算的欧式距离/当前速度
         # print(self.velocity,fvalue)
         return gvalue, fvalue, (self.pos[0] - father.pos[0], self.pos[1] - father.pos[1]), self.velocity
 
@@ -181,6 +195,7 @@ class AStar(object):
         self.snode.set_vel(V0)  # 设置初始速度
         self.enode = Node(pos_en, None)  # 用于存储路径规划的目标节点
         self.cnode = self.snode  # 用于存储当前搜索到的节点
+        self.open_num = 0
 
     def run(self):
         self.openlist.append(self.snode)
@@ -216,6 +231,7 @@ class AStar(object):
 
         minroute.append(self.snode.pos)
         minroute.reverse()
+        print("openlist max size:", self.open_num)
         return minroute
 
     def extend(self, cnode):
@@ -230,6 +246,8 @@ class AStar(object):
                 else:
                     node.set_fx(self.enode, cnode)
                     self.openlist.append(node)
+                    if (len(self.openlist) > self.open_num):
+                        self.open_num = len(self.openlist)
 
     def setBlock(self, blocklist):
         '''
@@ -327,18 +345,20 @@ def find_new_turn(routelist, blocklist):
 
 
 def main():
+    t = time.time()
     mapsize = tuple(map(int, input('请输入地图大小，以逗号隔开：').split(',')))
     pos_snode = tuple(map(int, input('请输入起点坐标，以逗号隔开：').split(',')))
     pos_enode = tuple(map(int, input('请输入终点坐标，以逗号隔开：').split(',')))
     myAstar = AStar(mapsize, pos_snode, pos_enode)
-    # blocklist = gen_blocks(mapsize[0], mapsize[1], pos_snode, pos_enode) #生成密且小的障碍物
-    blocklist = gen_blocks_big(mapsize[0], mapsize[1], pos_snode, pos_enode)  # 生成少且大的障碍物
+    blocklist = gen_blocks(mapsize[0], mapsize[1], pos_snode, pos_enode) #生成密且小的障碍物
+    # blocklist = gen_blocks_big(mapsize[0], mapsize[1], pos_snode, pos_enode)  # 生成少且大的障碍物
+    # blocklist = read_list_from_file("block.csv")
     myAstar.setBlock(blocklist)
     if myAstar.run() == 1:
         routelist = myAstar.get_minroute()
-        print(routelist)
         # turnlist, indexlist = get_turn(routelist)
         turnlist = find_new_turn(routelist, blocklist)
+        print(time.time() - t)
         draw_path(mapsize, blocklist, routelist, turnlist)
     else:
         print('路径规划失败！')
@@ -364,6 +384,32 @@ def gen_blocks(width, height, snode, enode):
     return blocklist
 
 
+def is_inrect(rect, point):
+    # resct:左下,_,_右上
+    print(rect, point)
+    if point[0] in range(rect[0][0], rect[3][0] + 1) and \
+            point[1] in range(rect[0][1], rect[3][1] + 1):
+        return True
+    return False
+
+
+def save_to_file(list, filename):
+    df = pd.DataFrame(list)
+    df.to_csv(filename)
+    print("保存成功")
+
+
+# 仅限二维点
+def read_list_from_file(filenmae):
+    df = pd.read_csv(filenmae, dtype=float)
+    dct_data = np.array(df.loc[:, :])
+    pxs = dct_data[:, 1]
+    pys = dct_data[:, 2]
+
+    list = [(x, y) for (x, y) in zip(pxs, pys)]
+    return list
+
+
 def gen_blocks_big(width, height, snode, enode):
     '''
     随机生成障碍物
@@ -374,23 +420,31 @@ def gen_blocks_big(width, height, snode, enode):
     i, blocklist = 0, []
     while (i < BLOCK_BIG_NUM):
         block = (randint(0, width - 1), randint(0, height - 1))
-        bord = randint(0, BLOCK_BORD)
-        if bord % 2 == 0: continue
+        bord = randint(BLOCK_BORD[0], BLOCK_BORD[1])
+        if bord % 2 != 0: continue
         if block[0] - bord / 2 >= 0 and block[0] + bord / 2 <= width \
                 and block[1] - bord / 2 >= 0 and block[1] + bord / 2 <= height:
             rect = [
-                (block[0] - (bord / 2 + CELL_WIDTH), block[1] - (bord / 2 + CELL_HEIGHT)),
-                (block[0] + (bord / 2 + CELL_WIDTH), block[1] - (bord / 2 + CELL_HEIGHT)),
-                (block[0] - (bord / 2 + CELL_WIDTH), block[1] + (bord / 2 + CELL_HEIGHT)),
-                (block[0] + (bord / 2 + CELL_WIDTH), block[1] + (bord / 2 + CELL_HEIGHT))
+                (block[0] - int(bord / 2), block[1] - int(bord / 2)),
+                (block[0] + int(bord / 2), block[1] - int(bord / 2)),
+                (block[0] - int(bord / 2), block[1] + int(bord / 2)),
+                (block[0] + int(bord / 2), block[1] + int(bord / 2))
             ]
-            if if_inPoly(rect, snode) or if_inPoly(rect, snode):
+            if is_inrect(rect, snode) or is_inrect(rect, enode):
                 continue
             else:
+                print(rect)
                 for x in range(int(block[0] - bord / 2), int(block[0] + bord / 2 + 1)):
                     for y in range(int(block[1] - bord / 2), int(block[1] + bord / 2 + 1)):
                         blocklist.append((x, y))
-                i = i + 1
+                i += 1
+
+    # blocklist = np.array(blocklist)
+    # plt.figure(figsize=(100, 100))
+    # plt.scatter(blocklist[:, 0], blocklist[:, 1], s=2800, c='k', marker='s')
+    # plt.savefig("block.png")
+    # save_to_file(blocklist, "block.csv")
+    # xxx = read_list_from_file("block.csv")
     return blocklist
 
 
